f3d5bf2de0d8eb7612ded749d15dec8327db57ad
Dexter Zhuang <dexter.zhuang@gmail.com>
2014-09-04 22:34:52 -0700

    fix bootstrap & gem issues

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,7 @@ module AbTestingToolkit
     # config.i18n.default_locale = :de
     
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.enabled = true
+    config.assets.compile = true
   end
 end
